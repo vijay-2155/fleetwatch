7519d9a2692f609253b57210b6144b8e56b42a20
Vijay-2155 <vijaykumartholeti2005@gmail.com>
2026-05-13 07:48:14 +0530

    Remove driver app from monorepo

--- driver_app/android/app/src/main/kotlin/com/fleettrack/fleet_tracker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fleettrack.fleet_tracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- driver_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:fleet_tracker/main.dart';
-
-void main() {
-  testWidgets('App builds without error', (WidgetTester tester) async {
-    await tester.pumpWidget(const FleetTrackerApp());
-    expect(find.text('Fleet Tracker'), findsOneWidget);
-  });
-}
